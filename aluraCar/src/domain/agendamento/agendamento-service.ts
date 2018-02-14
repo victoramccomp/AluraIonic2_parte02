142dc25b6673993fd039ddeb6591d73f1b494f3f
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2018-02-14 14:59:11 -0200

    Aula 02 - Reenviando agendamentos

--- aluraCar/src/domain/agendamento/agendamento-service.ts
+++ aluraCar/src/domain/agendamento/agendamento-service.ts
@@ -11,19 +11,38 @@ export class AgendamentoService {
 
     }
 
+    private _montarUri(agendamento: Agendamento) {
+
+        return `https://aluracar.herokuapp.com/salvarpedido?carro=${agendamento.carro.nome}&nome=${agendamento.nome}&preco=${agendamento.valor}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`;
+    }
+
     agendar(agendamento: Agendamento) {
         
+        let api = this._montarUri(agendamento);
+
         return this._dao.verificaAgendamentoDuplicado(agendamento)
             .then(existe => {
                 if(existe) 
                     throw new Error ('Agendamento já existente!');
                 
                 return this._http
-                    .get(`https://aluracar.herokuapp.com/salvarpedido?carro=${agendamento.carro.nome}&nome=${agendamento.nome}&preco=${agendamento.valor}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`)
+                    .get(api)
                     .toPromise()
                     .then(() => agendamento.confirmado = true, error => console.log(error))
                     .then(() => this._dao.salvar(agendamento))
                     .then(() => agendamento.confirmado);
             })
+    }
+
+    reagendar(agendamento: Agendamento) {
+
+        let api = this._montarUri(agendamento);
+        
+        return this._http
+            .get(api)
+            .toPromise()
+            .then(() => agendamento.confirmado = true, error => console.log(error))
+            .then(() => this._dao.salvar(agendamento))
+            .then(() => agendamento.confirmado);
     }
 }
--- aluraCar/src/pages/agendamentos/agendamentos.ts
+++ aluraCar/src/pages/agendamentos/agendamentos.ts
@@ -1,24 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { AgendamentoDao } from '../../domain/agendamento/agendamento-dao';
-import { Agendamento } from '../../domain/agendamento/agendamento'
+import { Agendamento } from '../../domain/agendamento/agendamento';
+import { AgendamentoService } from '../../domain/agendamento/agendamento-service';
 
 @Component({
-  templateUrl: 'agendamentos.html',
+    selector: 'page-agendamentos',
+    templateUrl: 'agendamentos.html',
 })
 export class AgendamentosPage {
 
-  public agendamentos: Agendamento[];
+    public agendamentos: Agendamento[];
 
-  constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams,
-    private _dao: AgendamentoDao
-  ) {
-    
-    this._dao
-      .listar()
-      .then(agendamentos => this.agendamentos = agendamentos);
-  }
+    constructor(
+        public navCtrl: NavController, 
+        public navParams: NavParams,
+        private _alertCtrl: AlertController,
+        private _dao: AgendamentoDao,
+        private _service: AgendamentoService
+    ) {
+        
+        this._dao
+            .listar()
+            .then(agendamentos => this.agendamentos = agendamentos);
+    }
 
+    reenviar(agendamento: Agendamento) {
+        
+        this._service
+            .reagendar(agendamento)
+            .then(confirmado => {
+
+                if (confirmado) {
+
+                    this._alertCtrl.create({
+                        title: 'Envio',
+                        subTitle: 'Agendamento reenviado com sucesso!',
+                        buttons: [{ text: 'Ok' }]
+                    }).present();
+                }
+            });
+    }
 }
